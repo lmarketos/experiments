fa05f76722039b87c3f80ed7864cd90aacf21b71
louis <git@marketos.com>
2025-04-06 20:22:15 -0600

    cpu-temp publisher with prometheus and grafana working

--- apps/server-stat-publisher/app.js
+++ apps/server-stat-publisher/app.js
@@ -21,30 +21,14 @@ function updateTemp()
     cpuTemp.set(Number(stdout));
   })
 }
-updateTemp()
+
+setInterval(updateTemp, 1000);
 // check cpu temp command from https://phoenixnap.com/kb/linux-cpu-temp
 // paste <(cat /sys/class/thermal/thermal_zone*/type) <(cat /sys/class/thermal/thermal_zone*/temp) | column -s $'\t' -t | sed 's/\(.\)..$/.\1°C/'
 const server = http.createServer(async (req, res) => {
       updateTemp();
       res.setHeader('Content-Type', register.contentType);
       res.end(await register.metrics());
-  // exec('cat /sys/class/thermal/thermal_zone2/temp', 
-  //     (error, stdout, stderr) => {
-  //   cpuTemp.set(25);
-  //   if (error) {
-  //     res.write('Error: ${error}')
-  //     return
-  //   }
-  //   if (req.url === '/metrics') {
-  //     res.setHeader('Content-Type', register.contentType);
-  //     res.write(await register.metrics());
-  //     res.end();
-  //   }
-  //   else {
-  //     res.write(stdout);
-  //     res.end();
-  //   }
-  // })
 })
 
 server.listen(port, function(error) {
